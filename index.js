28b9d3d4e63ef1857796628c369d391572f0626e
kyletsang <6854874+kyletsang@users.noreply.github.com>
2024-03-13 16:52:03 -0700

    feat: add ts config

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
-  extends: 'standard'
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      extends: 'standard',
+      rules: {
+        'space-before-function-paren': 'off'
+      }
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: 'standard-with-typescript',
+      rules: {
+        '@typescript-eslint/space-before-function-paren': 'off'
+      }
+    }
+  ]
 }
