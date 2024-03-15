dc731369109269fee70d87dc3474a8da95b60e51
kyletsang <6854874+kyletsang@users.noreply.github.com>
2024-03-15 14:33:52 -0700

    feat: more rules

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ module.exports = {
       extends: 'standard-with-typescript',
       rules: {
         '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/indent': 'off',
+        // This rule enforces some weird spacing on this:
+        // providerProps?:
+        // | {
+        //     copy?: LegalsProviderProps['copy']
+        //   }
+        // | undefined
+        '@typescript-eslint/key-spacing': 'off',
         '@typescript-eslint/no-confusing-void-expression': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/space-before-function-paren': 'off',
         '@typescript-eslint/strict-boolean-expressions': 'off'
       }
